6d45d12d6677c19936af9864d8add638e3fb201e
simonsenh <simonsenh@stu.otc.school.nz>
2021-08-06 12:53:39 +1200

    variables

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+computer_money = 100
+your_money = 100
+computer_bet = 0
+your_bet = 0
+
+cards_one = random.randint(1,13)
+cards_two = random.randint(1,13)
+cards_three = random.randint(1,13)
+cards_four = random.randint(1,13)
+cards_five = random.randint(1,13)
+your_card_one = random.randint(1,13)
+your_card_two = random.randint(1,13)
+computer_card_one = random.randint(1,13)
+computer_card_two = random.randint(1,13)
+
+print(your_card_one,your_card_two)
