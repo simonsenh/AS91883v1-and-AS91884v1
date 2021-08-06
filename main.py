2f07ac117cf88601e398b1c2b30ddb7d35f25781
simonsenh <simonsenh@stu.otc.school.nz>
2021-08-06 13:12:05 +1200

    rounds

--- main.py
+++ main.py
@@ -17,3 +17,47 @@ computer_card_one = random.randint(1,13)
 computer_card_two = random.randint(1,13)
 
 print(your_card_one,your_card_two)
+computer_bet += 10
+print("The computers bet is {}".format(computer_bet))
+your_bet = int(input("What is your bet?"))
+if your_bet > computer_bet:
+    computer_bet = your_bet
+if your_bet < computer_bet:
+    print("The computer won{}".format(your_bet))
+    computer_money += your_bet
+    your_money -= your_bet
+    computer_bet = 0
+    your_bet = 0
+print("The computers bet is {}".format(computer_bet))
+print(cards_one,cards_two,cards_three)
+your_bet = int(input("What is your bet?"))
+if your_bet > computer_bet:
+    computer_bet = your_bet
+if your_bet < computer_bet:
+    print("The computer won {}".format(your_bet))
+    computer_money += your_bet
+    your_money -= your_bet
+    computer_bet = 0
+    your_bet = 0
+print("The computers bet is {}".format(computer_bet))
+print(cards_one,cards_two,cards_three,cards_four)
+your_bet = int(input("What is your bet?"))
+if your_bet > computer_bet:
+    computer_bet = your_bet
+if your_bet < computer_bet:
+    print("The computer won {}".format(your_bet))
+    computer_money += your_bet
+    your_money -= your_bet
+    computer_bet = 0
+    your_bet = 0
+print("The computers bet is {}".format(computer_bet))
+print(cards_one,cards_two,cards_three,cards_four,cards_five)
+if your_bet > computer_bet:
+    computer_bet = your_bet
+if your_bet < computer_bet:
+    print("The computer won {}".format(your_bet))
+    computer_money += your_bet
+    your_money -= your_bet
+    computer_bet = 0
+    your_bet = 0
+print("The computers bet is {}".format(computer_bet))
