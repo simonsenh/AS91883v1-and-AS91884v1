b2cf430d3eb6c2c33b7998323ab1a5a3dadb80cf
simonsenh <simonsenh@stu.otc.school.nz>
2021-08-16 11:00:15 +1200

    Working progress

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,102 @@
+import math
+import random
+
 print("y")
 
+x = 2
+
+def math(x):
+    return x + 3
+
+print(math(x))
+
+input = 1
+
+def inputs(input):
+    alpha = input("What is a?(input number)")
+    if inputs > 1:
+        beta = input("What is b?(input number)")
+        if inputs > 2:
+            delta = input("What is c?(input number)")
+            if inputs > 3:
+                gamma = input("What is d?(input number)")
+                alpha = alpha * 1000000000000000
+                beta = beta * 1000000000
+                delta = delta * 100000
+                return alpha + beta + delta + gamma
+
+def alpha(output):
+   round()
+Beta:
+
+Gamma:
+Delta:
+main_equations=(“trigonometry_(1)”,“pythagoras_(2)”,“Quadratic_(3)”,power_functions_(4),“+,-,*,/_(5)”)
+trig_equations=(“sin(a)_(1)”,“sin-1(a)_(2)”,“cos(a)_(3)”,“cos(a)-1_(4)”,“tan(a)_(5)”,“tan(a)_(6)”)
+pythagoras_equations= (“a2+b2_(1)”,“a2-b2_(2)”)
+power_equations= (“√a_(1)”,“a2_(2)”,“a√b_(3)”,“ab_(4)”)
+function_equations = (“a+b_(1)”,“a-b_(2)”,“a*b_(3)”,“a/b_(4)”)
+number_list_main = (“1”,“2”,“3”,“4”,“5”)
+number_list_trig = (“1”,“2”,“3”,“4”,“5”,“6”)
+number_list _pythagoras= (“1”,“2”)
+number_list_power = (“1”,“2”,“3”,“4”)
+number_list_function = (“1”,“2”,“3”,“4”)
+OUTPUT(Main_Equations)
+Try:
+   equation_set = INPUT(“What equation set?(input number in bracket)”)
+   IF equation_set in number_list_main:
+      IF equation_set = 1
+         OUTPUT(trig_equations)
+         equation = INPUT(“What equation?(input number in bracket)”)
+         IF equation in number_list_trig:
+            IF equation = 1
+               OUTPUT(“sin(a)”)
+            IF equation = 2
+              OUTPUT(“sin-1(a)”)
+            IF equation = 3
+              OUTPUT(“cos(a)”)
+           IF equation = 4
+              OUTPUT(“cos(a)-1”)
+           IF equation = 5
+              OUTPUT”(tan(a)”)
+           IF equation = 5
+               OUTPUT”(tan(a)”)
+           IF equation = 6
+              OUTPUT”(tan(a)-1”)
+     IF equation_set = 2
+         OUTPUT(pythagoras_equations)
+         equation = INPUT(“What equation?(input number in bracket)”)
+         IF equation in number_list_pythagoras:
+            IF equation = 1
+               OUTPUT(“a2+b2”)
+            IF equation = 2
+              OUTPUT(“a2-b2”)
+     IF equation_set = 3
+         OUTPUT(“−b ± √(b2 − 4ac)/2a”)
+     IF equation_set = 4
+         OUTPUT(power_equations)
+         equation = INPUT(“What equation?(input number in bracket)”)
+         IF equation in number_list_power:
+            IF equation = 1
+               OUTPUT(“√a”)
+            IF equation = 2
+              OUTPUT(“a2”)
+            IF equation = 3
+              OUTPUT(“a√b”)
+            IF equation = 4
+              OUTPUT(“ab”)
+     IF equation_set = 5
+         OUTPUT(function_equations)
+         equation = INPUT(“What equation?(input number in bracket)”)
+         IF equation in number_list_function:
+            IF equation = 1
+               OUTPUT(“a+b”)
+            IF equation = 2
+              OUTPUT(“a-b”)
+            IF equation = 3
+              OUTPUT(“a*b”)
+            IF equation = 4
+              OUTPUT(“a/b”)
+
+Except ValueError:
+   OUTPUT(“Syntax error”)
