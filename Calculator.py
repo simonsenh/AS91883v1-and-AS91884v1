e288a2f6961750ff046a3feec76373abe8c59dc0
simonsenh <72894789+simonsenh@users.noreply.github.com>
2021-08-24 10:18:17 +1200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,67 +1,80 @@
 import math
-import random
 
+class output:
+  def __init__(output, a, b, c):
+    output.a = a
+    output.b = b
+    output.c = c
 
+def inputs(letters):
 
-def inputs(input):
-    alpha = input("What is a?(input number)")
-    if input > 1:
-        beta = input("What is b?(input number)")
-        if input > 2:
-            delta = input("What is c?(input number)")
-            if input > 3:
-                gamma = input("What is d?(input number)")
-                alpha = alpha * 1000000000000000
-                beta = beta * 1000000000
-                delta = delta * 100000
-                return alpha + beta + delta + gamma
+    alpha = ""
+    beta = ""
+    gamma = ""
+    if string == 0:
+      alpha = input("What is a?(input number)")
+      if letters > 1:
+         beta = input("What is b?(input number)")
+         if letters > 2:
+              gamma = input("What is c?(input number)")
 
-def alpha(output):
-   round(output, -2)
-
-def beta(output):
-    round(output, -2)
-
-def gamma(output):
-    round(output, -2)
-
-def delta(output):
-    round(output, -2)
+    Output1 = output(alpha,beta,gamma)
+    return Output1
 
 letters = 1
-main_equations = ("trigonometry_(1)","pythagoras_(2)","Quadratic_(3)","power_functions_(4)","+,-,*,/_(5)")
-trig_equations = ("sin(a)_(1)","sin-1(a)_(2)","cos(a)_(3)","cos(a)-1_(4)","tan(a)_(5)","tan(a)_(6)")
-pythagoras_equations = ("a2+b2_(1)","a2-b2_(2)")
-power_equations = ("√a_(1)","a2_(2)","a√b_(3)","ab_(4)")
-function_equations = ("a+b_(1)","a-b_(2)","a*b_(3)","a/b_(4)")
+string = 0
 number_list_main = ("1","2","3","4","5")
 number_list_trig = ("1","2","3","4","5","6")
 number_list_pythagoras = ("1","2")
 number_list_power = ("1","2","3","4")
 number_list_function = ("1","2","3","4")
-print(main_equations)
+print("[1] Trigonometry\n[2] Pythagoras\n[3] Quadratic\n[4] Power_functions \n[5] +,-,*,/")
+
 try:
-   equation_set = input("What equation set?(input number in bracket)")
+   equation_set = input("What equation set?(input number in square bracket):")
    if equation_set in number_list_main:
       if equation_set == "1":
-         print(trig_equations)
-         equation = input("What equation?(input number in bracket)")
+         print("[1] sin(a) \n[2] sin-1(a) \n[3] cos(a) \n[4] cos-1(a) \n[5] tan(a) \n[6] tan-1(a)")
+         equation = input("What equation?(input number in bracket):")
          if equation in number_list_trig:
             if equation == "1":
                 print("sin(a)")
+                Letters = 1
+                Output1 = inputs(letters)
+                answer = math.sin(math.radians(float(Output1.a)))
             elif equation == "2":
                 print("sin-1(a)")
+                letters = 1
+                string = 1
+                alpha = inputs(letters)
+                answer = math.asin(alpha)
             elif equation == "3":
                 print("cos(a)")
+                letters = 1
+                output = inputs(letters)
+                alpha = alpha(output)
+                answer = math.cos(alpha)
             elif equation == "4":
-                print("cos(a)-1")
+                print("cos-1(a)")
+                letters = 1
+                output = inputs(letters)
+                alpha = alpha(output)
+                answer = math.acos(alpha)
             elif equation == "5":
                 print("tan(a)")
+                letters = 1
+                output = inputs(letters)
+                alpha = alpha(output)
+                answer = math.tan(alpha)
             elif equation == "6":
-                print("tan(a)-1")
+                print("tan-1(a)")
+                letters = 1
+                output = inputs(letters)
+                alpha = alpha(output)
+                answer = math.atan(alpha)
       elif equation_set == "2":
-         print(pythagoras_equations)
-         equation = input("What equation?(input number in bracket)")
+         print("[1]a2+b2 \n[2]a2-b2")
+         equation = input("What equation?(input number in bracket):")
          if equation in number_list_pythagoras:
             if equation == "1":
                 print("a2 + b2")
@@ -70,8 +83,8 @@ try:
       elif equation_set == "3":
          print("(−b ± √(b2 − 4ac))/2a")
       elif equation_set == "4":
-         print(power_equations)
-         equation = input("What equation?(input number in bracket)")
+         print("[1]√a \n[2]a2 \n[3]a√b \n[4]ab9")
+         equation = input("What equation?(input number in bracket):")
          if equation in number_list_power:
             if equation == "1":
                print("√a")
@@ -82,8 +95,8 @@ try:
             elif equation == "4":
                print("ab")
       elif equation_set == "5":
-         print(function_equations)
-         equation = input("What equation?(input number in bracket)")
+         print("[1]a+b \n[2]a-b \n[3]a*b \n[4]a/b")
+         equation = input("What equation?(input number in bracket):")
          if equation in number_list_function:
             if equation == "1":
                print("a+b")
@@ -93,6 +106,6 @@ try:
                print("a*b")
             elif equation == "4":
                print("a/b")
-
+   print(answer)
 except ValueError:
   print("Syntax error")
